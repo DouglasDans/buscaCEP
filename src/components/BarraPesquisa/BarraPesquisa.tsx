6f56474dc8f2782d7db6bc67471cbd082a9ed98b
douglasdans <douglas.dans@gmail.com>
2024-01-18 19:10:26 -0300

    API | Adicionado código de implementação

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -1,9 +1,18 @@
+import { Api } from '@/api/api'
 import { Box, Input } from '@mui/joy'
+import { log } from 'console'
 import React from 'react'
 
 type Props = {}
 
 export default function BarraPesquisa({}: Props) {
+
+   const api = new Api();
+
+   api.getContent("cep", '08412180').then(res => {
+      console.log(res)
+   })
+
    return (
       <Box width={'800px'} height={'45px'}>
          <Input variant="soft" placeholder='Digite o CEP...' size="lg" />
