133026274e3132347e8a1e4945afc553d57992ec
douglasdans <douglas.dans@gmail.com>
2024-01-26 13:32:16 -0300

    BarraPesquisa | Implementação - Adicionado botão de pesquisa

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { Box, Input } from '@mui/joy'
-import React, { useEffect, useState } from 'react'
-import verifyCepAndFetch from "@/components/BarraPesquisa/AcessoAPI";
+import {Box, Button, Input} from '@mui/joy'
+import React from 'react'
+import {SearchRounded} from "@mui/icons-material";
 
 type Props = {
    setCepNumber(cep : string) : void
@@ -16,10 +16,30 @@ export default function BarraPesquisa({setCepNumber}: Props) {
 
    return (
       <Box width={'800px'} height={'45px'}>
-         <form onSubmit={handleSubmit}>
-            <Input  name='cep' variant="soft" placeholder='Digite o CEP...' size="lg" />
-            {/* <button type='submit'>a</button> */}
+         <form onSubmit={handleSubmit} style={styles.barraPesquisaContainer}>
+            <Input fullWidth name='cep' variant="soft" placeholder='Digite o CEP...' size="lg" />
+            <Box sx={styles.btnContainer}>
+               <Button sx={styles.button} variant={"plain"} type='submit'>
+                  <SearchRounded/>
+               </Button>
+            </Box>
          </form>
       </Box>
    )
+}
+
+const styles = {
+   barraPesquisaContainer : {
+      width: '100%',
+      display: 'flex',
+   },
+   btnContainer : {
+      display: 'flex',
+      alignItems: 'center'
+   },
+   button : {
+      borderRadius: '30rem',
+      width: '35px',
+      height: '35px'
+   }
 }
