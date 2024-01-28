4b564181ba503253389198d9f7c6e3c6de45cd2d
douglasdans <douglas.dans@gmail.com>
2024-01-28 16:51:24 -0300

    BarraPesquisa | Correção - Corrigido o posicionamento do botão de pesquisa

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Box, Button, Input} from '@mui/joy'
+import {Box, Button, Container, Input} from '@mui/joy'
 import React from 'react'
 import {SearchRounded} from "@mui/icons-material";
 
@@ -17,14 +17,25 @@ export default function BarraPesquisa({setCepNumber}: Props) {
    return (
       <Box width={'800px'} height={'45px'}>
          <form onSubmit={handleSubmit} style={styles.barraPesquisaContainer}>
-            <Input fullWidth name='cep' variant="soft" placeholder='Digite o CEP...' size="lg" />
-            <Box sx={styles.btnContainer}>
-               <Button sx={styles.button} variant={"plain"} type='submit'>
-                  <SearchRounded/>
-               </Button>
-            </Box>
+            <Input
+                endDecorator={<InputDecoratorButtons/>}
+                fullWidth
+                name='cep'
+                variant="soft"
+                placeholder='Digite o CEP...'
+                size="lg"
+            />
          </form>
       </Box>
+   )
+}
+function InputDecoratorButtons() {
+   return (
+       <Box sx={styles.btnContainer}>
+          <Button sx={styles.button} variant={"plain"} type='submit'>
+             <SearchRounded/>
+          </Button>
+       </Box>
    )
 }
 
@@ -35,11 +46,11 @@ const styles = {
    },
    btnContainer : {
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
    },
    button : {
       borderRadius: '30rem',
       width: '35px',
-      height: '35px'
+      height: '35px',
    }
 }
