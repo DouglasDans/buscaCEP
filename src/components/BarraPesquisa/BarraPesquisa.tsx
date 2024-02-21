278cf079fbce3c93596073c1a678db4581f58232
douglasdans <douglas.dans@gmail.com>
2024-02-21 19:51:41 -0300

    Adiciona ícone de loading quando é feita uma requisição

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import {Box, Button, Input} from '@mui/joy'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {SearchRounded} from "@mui/icons-material";
 import { useRouter } from 'next/navigation'
 
 export default function BarraPesquisa() {
    const router = useRouter()
    const [cep, setCep] = useState('')
+   const [loading, setLoading] = useState(false)
 
    function handleSubmit(e : any) {
       e.preventDefault()
+      setLoading(true)
       router.push(`/${e.target.cep.value}`)
    }
 
@@ -18,26 +20,34 @@ export default function BarraPesquisa() {
       <Box width={'800px'} height={'45px'}>
          <form onSubmit={handleSubmit} style={styles.barraPesquisaContainer}>
             <Input
-                endDecorator={<InputDecoratorButtons/>}
+                endDecorator={<InputDecoratorButtons loading={loading}/>}
                 fullWidth
                 onChange={(e) => {setCep(e.target.value)}}
                 value={cep}
-                type='number'
+                type='text'
                 name='cep'
                 variant="soft"
                 placeholder='Digite o CEP...'
                 size="lg"
+                sx={styles.inputNumber}
             />
          </form>
       </Box>
    )
 }
-function InputDecoratorButtons() {
+function InputDecoratorButtons({loading} : {loading: any}) {
+   console.log(loading)
    return (
        <Box sx={styles.btnContainer}>
-          <Button itemType={'submit'} sx={styles.button} variant={"plain"} type='submit'>
-             <SearchRounded/>
-          </Button>
+         {
+            loading && 
+            <Button loading variant="plain">
+               Plain
+            </Button>
+         }
+         <Button itemType={'submit'} sx={styles.button} variant={"plain"} type='submit'>
+            <SearchRounded/>
+         </Button>
        </Box>
    )
 }
@@ -55,5 +65,12 @@ const styles = {
       borderRadius: '30rem',
       width: '35px',
       height: '35px',
+   },
+   inputNumber: {
+      '::-webkit-inner-spin-button' : {
+         '-webkit-appearance': 'none',
+         '-moz-appearance': 'none',
+         'appearance': 'none'
+      }
    }
 }
